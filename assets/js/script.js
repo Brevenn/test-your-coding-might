7ef22965419fbf2d5915d5bf7f9b0e19df6d66b9
Brevenn <Brevenjh@gmail.com>
2022-07-18 12:25:38 -0500

    added more css and script.js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//  Questions JS Section  //
+
+var questions = [
+    {
+        title: "Which built-in methid returns the character at the specified index",
+        choices: ["characterAT()","getCharAt()","charAt()","None of the Above"],
+        answer: "charAt()"
+    },
+    {
+        title: "The condition in an if/else statement is enclosed with ________",
+        choices: ["parentheses","quotes","curly brackets","square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title:"Which of the following function of Number object returns a string value version of the current number?",
+        choices: ["toString()","toFixed()","toLocaleString()","toPrecision()"],
+        answer: "toString()"
+    },
+    {
+        title:"What is an attribute used in html to decorate content?",
+        choices: ["css","class","src","style",],
+        answer: "style"
+    },
+    {
+        title:"Which of the followeing html tags is NOT included in the beginning HEAD tag?",
+        choices: ["link","meta","title","header"],
+        answer: "header"
+    }
+]
+
+// Highscore Keeping Section //
