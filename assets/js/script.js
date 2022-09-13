245dbf27e4887344815dd6f56ebb47c84b587267
Brevenn <Brevenjh@gmail.com>
2022-09-13 17:28:30 -0500

    began questions array for the questions on the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question:"String values must be enclosed within _____ when being assigned to variables.",
+    choices:["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+    answer:"c. quotes"
+  },
+  {
+    question:"Inside which HTML element do we put the Javascript?",
+    choices:["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+    answer:"d. <script>"
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices:["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+    answer:"c. alerts"
+  },
+  {
+    question:"Arrays in JavaScript can be used to store ______.",
+    choices:["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+    answer: "b. other arrays"
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    choices:["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+    answer: "b. function myFunction()"
+  },
+  {
+    question:"How do you call a function named myFunction?",
+    choices:["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+    answer: "c. myFunction()"
+  },
+  {
+    question:
+    choices:["a. ", "b. ", "c. ", "d. "],
+    answer:
+  },
+  {
+    question:
+    choices:["a. ", "b. ", "c. ", "d. "],
+    answer:
+  },
+  {
+    question:
+    choices:["a. ", "b. ", "c. ", "d. "],
+    answer:
+  },
+  {
+    question:
+    choices:["a. ", "b. ", "c. ", "d. "],
+    answer:
+  }
+];
